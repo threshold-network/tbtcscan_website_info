a08787f975fa5f501d938044539aee271607607f
evandrosaturnino <evandro.saturnino@gmail.com>
2024-01-19 11:51:06 -0300

    refactor: rename subgraph client

--- src/utils/Cons.jsx
+++ src/utils/Cons.jsx
@@ -4,7 +4,7 @@ export const TESTNET_API = "https://api.thegraph.com/subgraphs/name/suntzu93/thr
 export const RPC_ETH_MAINNET = import.meta.env.VITE_RPC_ETH_MAINNET;
 export const RPC_ETH_GOERLI = "https://eth-goerli.g.alchemy.com/v2/BS3qcnNmATIAa9rI7xFMmpyB-RHg_dAm"
 
-export const MAINNET_AP_BALANCE = import.meta.env.VITE_MAINNET_AP_BALANCE;
+export const MAINNET_AP_BALANCE = "https://api.etherscan.io/api?module=account&action=balance&tag=latest&apikey=BIRHCE1D6MEMBR9UN4QJSTNJ5WV58YH12Y&address=";
 export const GOERLI_API_BALANCE =
     "https://api-goerli.etherscan.io/api?module=account&action=balance&tag=latest&apikey=BIRHCE1D6MEMBR9UN4QJSTNJ5WV58YH12Y&address=";
 
